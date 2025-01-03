c201172e5f5c62a4f6c64888fb6c9a145967970a
Jacob Ebey <jacob.ebey@live.com>
2025-01-03 01:15:18 -0800

    add favicon and redirect

--- src/app/todo/todo.tsx
+++ src/app/todo/todo.tsx
@@ -1,9 +1,33 @@
+import {
+  destoryCookieSession,
+  getCookieSession,
+  redirect,
+} from "framework/server";
+import { Button } from "~/components/ui/button";
+
 export default function Todo() {
+  const loggedIn = !!getCookieSession("userId");
+
+  if (!loggedIn) {
+    return redirect("/");
+  }
+
   return (
     <main className="container w-full mx-auto px-4 md:px-6 py-16">
       <div className="typography">
         <h1>Todo</h1>
         <p>Todo page</p>
+
+        <form
+          action={() => {
+            "use server";
+            destoryCookieSession();
+          }}
+        >
+          <p>
+            <Button type="submit">Logout</Button>
+          </p>
+        </form>
       </div>
     </main>
   );
